0d17672670c0580163b45fa4af6fda9f5148ffea
Chelle77322 <serenynadarra@gmail.com>
2021-05-06 17:57:11 +0930

    Delete directory directory
    Surplus directory folder everything has been moved to the root

--- directory/src/utils/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const Api = {
-  getUsers: function () {
-    return axios.get("https://randomuser.me/api?results=200&nat=au");
-  },
-};
-console.log(Api);
-export default Api;
