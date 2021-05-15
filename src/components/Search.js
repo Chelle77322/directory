90173bdd816962062d65eeef0519b8bf20fbd090
Chelle77322 <serenynadarra@gmail.com>
2021-05-15 21:53:04 +0930

    Rewriting of the script file Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import "../styles/Search.css";
-
-function Search ({ handleSearchChange})
-{
-    return(
-        <div className = "searchBox">
-            <form className = "form-inline">
-                <input className = "form-control"
-                type = "search"
-                placeholder = "Search for User"
-                aria-label = "User Search"
-                onChange = {e => handleSearchChange(e)}/>
-            </form>
+import React, { useState } from 'react';
+import Api from " ../utils/Api";
+const Search = () => { 
+const state = useState(Api);
+    return (
+     <div className = "searchbox">
+         <div className = "input-group">
+         <div className = "input-group-prepend">
+            <span className = "input-group-text" id = "">Search</span>
+         </div>
+         <input className = "form-control mr-sm-2" type = "search" placeholder = "Employee Name" aria-label = "Search"
+         onChange={e => state.handleSearchChange(e)}/>
+            </div>
         </div>
     );
 }
