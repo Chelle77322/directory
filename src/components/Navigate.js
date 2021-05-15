26780e655f5646bb1abb708699cb43f4227266e6
Chelle77322 <serenynadarra@gmail.com>
2021-05-15 22:00:48 +0930

    Rewriting the Navigation component

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Search from "./Search.js";
 import "../styles/Navigate.css";
 
-function Navigate ({ handleSearchChange}){
+function Navigate (){
     return (
-        <Navigate className = "navbar navbar-expand navbar-light bg-light">
-            <div className = "navbar-collapse row" id = "navbarNav">
-            <Search handleSearchChange = {handleSearchChange}/>
-            </div>
-        </Navigate>
+     <nav className = "navbar navbar-expand-lg navbar-light bg-light">
+        <button className = "navbar-toggler" type = "button" data-toggle = "collapse"
+        data-target = "#navbarNav"
+        aria-controls = "navbarNav"
+        aria-expanded = "false"
+        aria-label = "Toggle navigation">
+            <span className = "navbar-toggler-icon"/>
+        </button>
+        <div className = "collapse navbar-collapse row" id = "navbarNav">
+        < div className = "search-area col-4">
+            <Search />
+        </div>
+        </div>
+     </nav>
     );
 }
 export default Navigate;
