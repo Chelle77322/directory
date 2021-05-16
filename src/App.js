f8237a5991ffd4edecbca79c6c30306a6ce07790
Chelle77322 <serenynadarra@gmail.com>
2021-05-16 21:02:01 +0930

    Rewrite of App.js as it was full of unneccesary code

--- src/App.js
+++ src/App.js
@@ -1,69 +1,18 @@
-import React, {useState} from 'react';
-import {Row, Col} from 'reactstrap';
-import Search from "./components/Search";
-import Banner from "./components/Banner";
-import DataTable from "./components/DataTable";
-import {Container} from "./styles/Container.css";
-import Api from './utils/Api';
+import React from 'react';
+import Container from './components/Container';
+import Wrapped from './components/Wrapped';
+import Banner from './components/Banner';
 
+import './App.css';
 
-const App = () => {
-    const [data, setData] = useState({
-        employees: Api
-    })
-    const {employees} = data
-
-    const handleChange = (e) => {
-        e.preventDefault();
-        const searchInput = e.target.value.toLowerCase();
-        let newemployees = [];
-        if (searchInput !== " "){
-            newemployees = Api.filter(employee => {
-                return (
-                    searchInput === employee.name.first.toLowerCase().slice(0, searchInput.length)
-                )
-            })
-        } else {
-            newemployees = Api;
-        }
-        setData ({employees:newemployees})
-
-        }
-        const handleOnClick = () =>{
-            setData({ employees: employees.sort(sortByName)})
-        }
-        function sortByName( a , b){
-        const nameA = a.name.first.toLowerCase();
-        const nameB = b.name.first.toLowerCase();
-
-        let comparison = 0;
-        if (nameA > nameB){
-            comparison = 1;
-        } else if (nameA < nameB){
-            comparison = -1;
-        }
-        return comparison
-        }
-        return (
-            <Container>
-                <Row>
-                    <Col>
-                    <Banner></Banner>
-                    </Col>
-                </Row>
-            <Row>
-                <Col sm = "12" md= {{size: 6, offset: 3}}>
-                <Search handleChange = {handleChange}>
-
-                </Search>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                <DataTable employees = {employees} onClick = {handleOnClick}></DataTable>
-                </Col>
-            </Row>
-            </Container>
-        );
-    }
+function App() {
+    return (
+        <div className ="App">
+        <Wrapped>
+            <Banner />
+            <Container />
+        </Wrapped>
+        </div>
+    );
+}
 export default App;
