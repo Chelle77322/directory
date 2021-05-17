9f3a6f4992376d6205ebd75bd631a3611fae80e1
Chelle77322 <serenynadarra@gmail.com>
2021-05-17 13:47:09 +0930

    Getting Search function to work and not error out

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React, { useContext } from "react";
+import "../styles/Search.css";
 import Context from "../utils/Context";
-const Search = () => { 
-const state = useState(Context);
+
+const Search = () =>{
+const data = useContext(Context);
     return (
-     <div className = "searchbox">
-         <div className = "input-group">
-         <div className = "input-group-prepend">
-            <span className = "input-group-text" id = "">Search</span>
-         </div>
-         <input className = "form-control mr-sm-2" type = "search" placeholder = "Employee Name" aria-label = "Search"
-         onChange={e => state.handleSearchChange(e)}/>
-            </div>
-        </div>
+    <div className = "searchbox">
+     <div className = "imput-group">
+         <div className = "input-group-prepend"> <span className = "input-group-text"id = " ">Search</span>
+     </div>
+       
+    <input className = "form-control mr-sm-2" type = "search" placeholder = "name" aria-label = "Search"
+    onChange={e => data.handleSearchChange(e)}
+    />
+     </div>
+     </div>
     );
 }
 export default Search;
